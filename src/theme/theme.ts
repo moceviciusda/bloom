847eeda8e800c96af6e31988786e8652593b7e46
moceviciusda <moceviciusdarius@gmail.com>
2024-03-13 10:49:39 +0200

    remove component themes

---
+++ src/theme/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const styles = {
+  global: {
+    ':not(.chakra-dont-set-collapse) > .chakra-collapse': {
+      overflow: 'initial !important',
+    },
+  },
+};
+
+const config = { initialColorMode: 'light' };
+
+const theme = extendTheme({ config, styles });
+
+export default theme;
